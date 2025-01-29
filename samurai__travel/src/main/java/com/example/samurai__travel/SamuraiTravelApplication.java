a229a0d9c6fee3d0de2f46d86bafb77228373fe6
mmcarbonara <mmsloth1997@gmail.com>
2025-01-29 23:39:17 +0900

    add

---
+++ samurai__travel/src/main/java/com/example/samurai__travel/SamuraiTravelApplication.java
@@ -0,0 +1,13 @@
+package com.example.samurai__travel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamuraiTravelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamuraiTravelApplication.class, args);
+	}
+
+}
